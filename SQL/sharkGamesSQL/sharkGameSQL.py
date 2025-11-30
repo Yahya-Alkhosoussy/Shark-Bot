444c88a2b45a4287accd659a79acc8776a10ce6b
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2025-11-30 14:15:04 +0000

    added commit to the SQL side, hopefully that fixes the functionality of the remove net function

--- SQL/sharkGamesSQL/sharkGameSQL.py
+++ SQL/sharkGamesSQL/sharkGameSQL.py
@@ -375,6 +375,7 @@ def remove_net_use(username: str, net: str, net_uses: int):
         rowid = row[0]
         cursor.execute(f"UPDATE '{username} dex' SET net_uses={net_uses} WHERE rowid = {rowid}")
         print("removed 1 net use")
+        connection.commit()
 
 def is_net_available(username: str, net: str):
 
