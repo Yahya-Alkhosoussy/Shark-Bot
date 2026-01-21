f00f1459aa5709faa801dd3e98ec1dccd711f9d1
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-01-21 12:10:25 +0000

    Added compatibility with real time changes to levelling config

--- loops/levellingloop/levellingLoop.py
+++ loops/levellingloop/levellingLoop.py
@@ -32,21 +32,49 @@ async def create_rank_card(user: discord.Member, rank: int, level: int, xp: int,
     """
 
     # Create a blank image
-    img = Image.new(mode='RGB', size=(900, 250), color=(253, 61, 181)) # The background colour is magenta
+    black: tuple = (0, 0, 0)
+    img = Image.open(r"loops\levellingloop\images\leveling up background.png")
+    img = img.convert("RGBA")
     draw = ImageDraw.Draw(img) # Creates the drawing tool that will be working on the image
 
     profile_picture_url = user.display_avatar.url
     response = requests.get(profile_picture_url) # gets the image from the web
     profile_picture = Image.open(BytesIO(response.content)) # This opens the image as a PIL image object and doesn't download the image, as BytesIO stores it in memory
-    profile_picture = profile_picture.resize(size=(150, 150))
+    profile_picture = profile_picture.resize(size=(192, 179))
 
     # Make the profile picture circular
-    mask = Image.new(mode='L', size=(150, 150), color=0)
+    mask = Image.new(mode='L', size=(192, 179), color=0)
     mask_draw = ImageDraw.Draw(mask)
-    mask_draw.ellipse(xy=(0, 0, 150, 150), fill=255) # xy is in the format of x1, y1, x2, y2
+    mask_draw.ellipse(xy=(0, 0, 192, 179), fill=255) # xy is in the format of x1, y1, x2, y2
 
     # Past the profile picture into the card
-    img.paste(im=profile_picture, box=(30, 50), mask=mask)
+    img.paste(im=profile_picture, box=(79, 49), mask=mask)
+
+    # Create overlay
+    overlay = Image.new('RGBA', img.size, (0, 0, 0, 0))
+    overlay_draw = ImageDraw.Draw(overlay)
+    
+    # colours in RGB
+    skin_white: tuple = (255, 240, 236, 128) #RGB then opacity
+    purple: tuple = (182, 59, 150, 255)
+    grey: tuple = (60, 60, 60, 229)
+
+    # draw opaque background behind rank and level
+    level_x, level_y = 565, 75
+    level_width, level_height = 220, 44
+    overlay_draw.rectangle(xy=[level_x, level_y, level_x + level_width, level_y + level_height], fill=skin_white, outline=purple)
+
+    rank_x, rank_y = 620, 15
+    rank_width, rank_height = 165, 46
+    overlay_draw.rectangle(xy=[rank_x, rank_y, rank_x + rank_width, rank_y + rank_height], fill=skin_white, outline=purple)
+
+    # Draw the background of the progress bar
+    bar_x, bar_y = 333, 138
+    bar_width, bar_height = 451, 51
+    overlay_draw.rectangle(xy=[bar_x, bar_y, bar_x + bar_width, bar_y + bar_height], fill=grey )
+
+    # compose it into the main image
+    img = Image.alpha_composite(img, overlay)
 
     # Load fonts
     try: 
@@ -58,32 +86,23 @@ async def create_rank_card(user: discord.Member, rank: int, level: int, xp: int,
         font_medium = ImageFont.load_default()
         font_small = ImageFont.load_default()
         logging.warning(f"[LEVELING SYSTEM] Unable to load font 'arial.ttf' using default.")
-    
-    # colours in RGB
-    white: tuple = (255, 255, 255)
-    pale_lavendar: tuple = (230, 200, 255)
-    cyan: tuple = (0, 255, 255)
-    grey: tuple = (60, 60, 60)
 
-    # Draw username
-    draw.text(xy=(200, 60), text=user.name, fill=white, font=font_medium)
+    # to be able to place the text on the background
+    draw = ImageDraw.Draw(img)
 
     # Draw rank and level
-    draw.text(xy=(750, 30), text=f"RANK #{rank}", fill=pale_lavendar, font=font_small)
-    draw.text(xy=(750, 60), text=f"LEVEL {level}", fill=pale_lavendar, font=font_medium)
+    draw.text(xy=(650, 30), text=f"RANK #{rank}", fill=purple, font=font_small)
+    draw.text(xy=(600, 80), text=f"LEVEL {level}", fill=purple, font=font_medium)
 
-    # Draw XP 
-    draw.text(xy=(750, 120), text=f"{xp:,}", fill=cyan, font=font_medium) # the ':,' makes it a 1000 comma separator
+    # # Draw XP 
+    # draw.text(xy=(750, 120), text=f"{xp:,}", fill=purple, font=font_medium) # the ':,' makes it a 1000 comma separator
     
-    # Draw the background of the progress bar
-    bar_x, bar_y = 200, 140
-    bar_width, bar_height = 550, 30
-    draw.rectangle(xy=[bar_x, bar_y, bar_x + bar_width, bar_y + bar_height], fill=grey, outline=(80, 80, 80))
+    
 
     # draw progress bar filled
     progress = xp / xp_needed
     fill_width = int(bar_width * progress)
-    draw.rectangle(xy=[bar_x, bar_y, bar_x + fill_width, bar_y + bar_height], fill=cyan)
+    draw.rectangle(xy=[bar_x, bar_y, bar_x + fill_width, bar_y + bar_height], fill=purple)
 
     # Save to BytesIO to send as discord file
     buffer = BytesIO()
@@ -124,8 +143,11 @@ class levelingLoop:
 
     
     async def message_handle(self, message: discord.Message):
+        config = RY.read_config(CONFIG_PATH) # This is here too just so it can check for live changes
         boost_event  = config.get("boost")
+        print(boost_event)
         boost_amount = config.get("boost amount")
+        print(boost_amount)
 
         username = message.author.name
 
@@ -143,20 +165,20 @@ class levelingLoop:
             channel = message.channel
             message_to_send: str | None
             if level == 1:
-                message_to_send = f"""Weaving through sunlit coral gardens where each wave catches the light, you enter the Shimmering Shallow Reefs.
+                message_to_send = f"""_Weaving through sunlit coral gardens where each wave catches the light, you enter the Shimmering Shallow Reefs._
 Keep swimming and chatting — the sparkling waters of the Kelp Forest are just ahead. You now have more areas to explore! {message.author.mention}"""
             elif level == 2:
-                message_to_send = f"""Mid-depth currents sway the kelp, and tiny flecks of light dance across the water.
+                message_to_send = f"""_Mid-depth currents sway the kelp, and tiny flecks of light dance across the water._
 Swim a little further and you'll find your way into the wide, open ocean - so many new friends! {message.author.mention}"""
             elif level == 3:
-                message_to_send = f"""These waters are vast, the currents gentle but sparkling all around you.
+                message_to_send = f"""These waters are vast, the currents gentle but sparkling all around you._
 Every movement leaves a trail of glittering ripples.
 Keep exploring — the Twilight Zone waits. {message.author.mention}"""
             elif level == 4:
-                message_to_send = f"""Sunlight fades, but you see tiny bioluminescent glimmers dance along the darkened waves before you.
+                message_to_send = f"""_Sunlight fades, but you see tiny bioluminescent glimmers dance along the darkened waves before you._
 Chat, swim, and level up to reach the Abyssal Deep, the ocean's most secret and glittering depths. {message.author.mention}"""
             elif level == 5:
-                message_to_send = f"""You've reached the deepest glittering waters, where rare and magical creatures dwell. A siren's song beckons from down in the deep.
+                message_to_send = f"""_You've reached the deepest glittering waters, where rare and magical creatures dwell. A siren's song beckons from down in the deep._
 The currents here shimmer with bioluminescent magic — irl-pics, selfies, venting, and more are now unlocked.
 Swim thoughtfully, respect the depths, and enjoy your sparkling new habitat. {message.author.mention}"""
             else:
@@ -173,6 +195,6 @@ Swim thoughtfully, respect the depths, and enjoy your sparkling new habitat. {me
 
         card = await create_rank_card(user=message.author, rank=rank, level=level, xp=exp, xp_needed=exp_needed)
 
-        await message.reply(f"You currently are level {level} and here's your card!", file=card)
+        await message.reply(f"You currently are level {level} with {exp} xp and here's your card!", file=card)
 
             
