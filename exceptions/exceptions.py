da0d25c91691e369b82effaa2249dc6d31c87346
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-02-12 02:31:17 +0000

    got custom exceptions running as well as error codes to go with it.

---
+++ exceptions/exceptions.py
@@ -0,0 +1,20 @@
+class SharkBotException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class ItemNotFound(SharkBotException):
+    def __init__(self, message: str, error_code: int):
+        """
+        Docstring for __init__
+
+        :param message: Error Message
+        :type message: str
+        :param error_code: The Error code for the specific error (check exceptions.md)
+        :type error_code: int
+        """
+        super().__init__(message)
+        self.error_code = error_code
+
+    def __str__(self):
+        return f"{self.message} (Error code: {self.error_code})"
