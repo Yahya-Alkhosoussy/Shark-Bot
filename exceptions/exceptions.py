7d58c3bda68538d338a7e12594ee68910a88bf4e
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-02-25 20:21:43 +0000

    Added format error exception class for gifs and future relevant errors

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -39,3 +39,13 @@ class BirthdateFormatError(SharkBotException):
 
     def __str__(self):
         return f"{self.message} (Error code: {self.error_code})"
+
+
+class FormatError(SharkBotException):
+    def __init__(self, message: str, error_code: int):
+        super().__init__(message)
+        self.message = message
+        self.error_code = error_code
+
+    def __str__(self):
+        return f"{self.message} (Error code: {self.error_code})"
