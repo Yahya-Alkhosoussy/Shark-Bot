7f6de451fa4ae279a01728379776ead262da0cb6
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-03-12 05:02:57 +0000

    made the basic SQL file for saving all the mod actions

---
+++ SQL/modActions/modActionsSQL.py
@@ -0,0 +1,69 @@
+import sqlite3
+from datetime import datetime
+
+conn = sqlite3.connect(r"databases/twitch_stuff")
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS bans
+            (
+                id PRIMARY KEY,
+                banned_user TEXT UNIQUE,
+                reason TEXT,
+                mod_that_banned_them TEXT,
+                when_banned TEXT
+            )""")
+
+cur.execute("""CREATE TABLE IF NOT EXISTS timeouts
+            (
+                id PRIMARY KEY,
+                timed_out_user TEXT,
+                reason TEXT,
+                mod_that_timed_them_out TEXT,
+                duration INTEGER,
+                when_timed_out TEXT
+            )""")
+
+
+def add_ban(user: str, reason: str | None, mod: str, when: datetime):
+    when_str = when.strftime(r"%Y-%m-%d %H:%M")
+    cur.execute(
+        "INSERT OR IGNORE INTO bans (banned_user, reason, mod_that_banned_them, when_banned) VALUES (?, ?, ?, ?)",
+        (user, reason, mod, when_str),
+    )
+
+
+def add_timeout(user: str, reason: str | None, mod: str, when: datetime, duration: int):
+    when_str = when.strftime(r"%Y-%m-%d %H:%M")
+    cur.execute(
+        """INSERT OR IGNORE INTO timeouts (time_out_user, reason, mod_that_timed_them_out, duration, when_timed_out)
+        VALUES (?, ?, ?, ?, ?)""",
+        (user, reason, mod, duration, when_str),
+    )
+
+
+def get_bans(amount: int) -> list[tuple[str, str, str, str]]:
+    cur.execute("SELECT * FROM bans ORDER BY when_banned DESC")
+    rows = cur.fetchall()
+    to_return: list[tuple[str, str, str, str]] = []
+    i = 0
+    for row in rows:
+        if i != amount:
+            to_return.append(row)
+            i += 1
+        else:
+            break
+    return to_return
+
+
+def get_timeouts(amount: int) -> list[tuple[str, str, str, int, str]]:
+    cur.execute("SELECT * FROM timeouts ORDER BY when_timed_out DESC")
+    rows = cur.fetchall()
+    to_return: list[tuple[str, str, str, int, str]] = []
+    i = 0
+    for row in rows:
+        if i != amount:
+            to_return.append(row)
+            i += 1
+        else:
+            break
+    return to_return
