0c44468378250a42cdfd5fcc46c864d98bb4660d
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-03-12 05:02:32 +0000

    Added an auth exception + code

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -49,3 +49,13 @@ class FormatError(SharkBotException):
 
     def __str__(self):
         return f"{self.message} (Error code: {self.error_code})"
+
+
+class AuthError(SharkBotException):
+    def __init__(self, message: str, error_code: int):
+        super().__init__(message)
+        self.message = message
+        self.error_code = error_code
+
+    def __str__(self):
+        return f"{self.message} (Error code: {self.error_code})"
