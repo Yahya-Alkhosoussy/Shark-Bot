db103cde8c15c0b5bc39b2cc773661f6b8938f5e
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-03-16 23:24:46 +0000

    the file to run for easy set up

---
+++ setup.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+
+
+def install():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    subprocess.check_call([sys.executable, "-m", "playwright", "install", "chromium"])
+    print("Setup complete!")
+
+
+if __name__ == "__main__":
+    install()
