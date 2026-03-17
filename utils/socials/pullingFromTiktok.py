511fe1afc5ac430694e50c633b3234e542f28a74
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-03-17 06:41:04 +0000

    changed the function to return the video links

--- utils/socials/pullingFromTiktok.py
+++ utils/socials/pullingFromTiktok.py
@@ -3,16 +3,17 @@ import asyncio
 from TikTokApi import TikTokApi
 
 
-async def get_latest_videos():
+async def get_latest_videos() -> list[str]:
+    "grabs all of the links of the past 10 videos from a certain tiktok user"
     async with TikTokApi() as api:
         await api.create_sessions(num_sessions=1, sleep_after=3, headless=False)
 
         user = api.user("sharkocalypse")
+        video_links: list[str] = []
         async for video in user.videos(count=10):
-            print(f"Video ID: {video.id}")
-            print(f"Description: {video.as_dict['desc']}")
-            print(f"Link: https://www.tiktok.com/@sharkocalypse/video/{video.id}")
-            print("-------------------------------------------------------------")
+            video_links.append(f"https://www.tiktok.com/@sharkocalypse/video/{video.id}")
+
+        return video_links
 
 
 asyncio.run(get_latest_videos())
