1072d7767bb01a49d0b23aaad1d354c37180044a
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-03-17 10:47:57 +0000

    added a way to get all the users from the SQL table

--- SQL/socialMedia/twitchLive.py
+++ SQL/socialMedia/twitchLive.py
@@ -34,7 +34,11 @@ def get_live_status(id: str | int) -> bool:
         cur.execute("SELECT live_status FROM twitch_users WHERE discord_id=?", (id,))
         return cur.fetchone()[0]
     cur.execute("SELECT live_status FROM twitch_users WHERE twitch_username=?", (id,))
-    return cur.fetchone()[0]
+    return bool(cur.fetchone()[0])
+
+def get_users() -> list[str]:
+    cur.execute("SELECT twitch_username FROM twitch_users")
+    return cur.fetchall()
 
 def get_custom_message(twitch_username: str):
     cur.execute("SELECT custom_message FROM twitch_users WHERE twitch_username=?", (twitch_username,))
