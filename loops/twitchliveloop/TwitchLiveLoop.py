22c040d56bac4aa02a551801c8526445bbb5d1eb
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-03-18 12:14:53 +0000

    finalised the loop

--- loops/twitchliveloop/TwitchLiveLoop.py
+++ loops/twitchliveloop/TwitchLiveLoop.py
@@ -5,7 +5,7 @@ from discord.ext import tasks
 
 from SQL.socialMedia.twitchLive import get_custom_message, get_live_status, get_users, update_live_status
 from utils.core import AppConfig
-from utils.pullingFromTwitch import is_live
+from utils.pullingFromTwitch import get_profile_picture, get_stream_details, is_live
 
 
 class TwitchLiveLoop:
@@ -31,7 +31,29 @@ class TwitchLiveLoop:
                     update_live_status(username=user, status=new_live_status)
                     live_link = f"www.twitch.tv/{user}"
                     custom_message = get_custom_message(user)
-                    embed_to_send = discord.Embed()
+                    details = get_stream_details(user)
+                    if details is not None:
+                        title, game_name = details
+                    else:
+                        continue
+                    embed_to_send = discord.Embed(
+                        title=f"{user} is live on Twitch! Go check them out!",
+                        colour=discord.Color(0xF6A6BB),
+                        description=f"[{title}]({live_link})",
+                    )
+                    profile_url = get_profile_picture(username=user)
+                    embed_to_send.set_thumbnail(url=profile_url)
+                    embed_to_send.add_field(name="Game", value=game_name)
+                    view = discord.ui.View()
+                    view.add_item(discord.ui.Button(label="Watch Stream", url=live_link))
+                    channel_id = self.config.get_channel_id("shark squad", "live")
+                    guild = self.bot.get_guild(guild_id)
+                    if guild is None:
+                        logging.error("GUILD IS NOT FOUND")
+                        continue
+                    channel = guild.get_channel(channel_id)
+                    if isinstance(channel, discord.TextChannel):
+                        await channel.send(content=custom_message, embed=embed_to_send, view=view)
 
         loop = tasks.loop(minutes=2, reconnect=True)(_tick)
         guild_name = self.config.guilds[guild_id]
