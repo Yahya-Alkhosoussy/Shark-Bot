7c95250727060f0cf33a69e4eb937b9a67e0eae2
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-03-23 12:21:11 +0000

    created the file and put the questions and the associated multiple choice answers

---
+++ modApplication/ModQuestions.py
@@ -0,0 +1,29 @@
+"""Questions:
+1. What is your twitch username? (open ended)
+2. What days are you available on? (multiple choice, multiple answers)
+3. Are you open to community projects? This can include leading specific games, creating events, movie nights, etc. (Y/N)
+4. Are you looking to mod primarily discord, live stream, or both? (multiple choice)
+5. How often would you say you engage with the community on twitch or in discord? (multiple choice)
+6. We do allow sensitive topics like religion, politics, sexuality, gender, etc. in the appropriate channels. Mods are expected
+to be available even for uncomfortable topics, and to intervene when needed. Are you willing to moderate potentially
+uncomfortable topics and remained unbiased? (Y/N)
+7. Have you worked as a twitch/discord moderator before? If yes, how did you support the streamer/community?
+(Y/N, if Y open ended)
+8. Do you consider yourself a person who can handle things quickly, and be confident in their decisions when provided adequate
+resources and a team? (Y/N)
+9. Many people are fine handling positive interactions but freeze or disappear when they have to handle a negative interaction.
+Will you be able to handle yourself in a mature way and keep an open line of dialogue, even if the subject is uncomfortable?
+(Y/N)
+10. Abuse of power/making decisions without others/overriding other mods can lead to immediate termination of your role, and if
+bad enough, a ban. Is that understood? (Y/N)
+11. Certain behaviors are expected of mods such as handling disagreements privately, being available for individuals to ask
+questions, not allowing yourself to be dragged into an argument/shit storm in chat, etc. Can you hold yourself to that standard?
+(Y/N)
+12. Finally, why do you want to mod for Shark? (open ended)
+"""
+
+"""Multiple choice answers:
+2. A. Monday B. Tuesday C. Wednesday D. Thursday E. Friday F. Saturday G. Sunday
+4. A. Discord B. Twitch C. Both
+5. A. Multiple times a day B. Atleast once a day C. Several times a week D. a couple times a week E. Not frequently at all
+"""
