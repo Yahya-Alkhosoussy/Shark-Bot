e990b03d597323b7b251219456732c12140b9331
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-03-23 12:11:00 +0000

    moved stuff in their proper places to open a ticket

--- modApplication/MyView.py
+++ modApplication/MyView.py
@@ -64,87 +64,85 @@ class MyView(discord.ui.View):
             await interaction.message.edit(
                 embed=embed, view=MyView(bot=self.bot)
             )  # This will reset the SelectMenu in the ticket channel
-            guild = self.bot.get_guild(guild_id)
-            assert guild
-            category_ids = config.categories[guild_name]
+        guild = self.bot.get_guild(guild_id)
+        assert guild
+        category_ids = config.categories[guild_name]
 
-            role_ids = config.roles[guild_name]
-            mod_role = guild.get_role(role_ids["mods"])
+        role_ids = config.roles[guild_name]
+        mod_role = guild.get_role(role_ids["mods"])
 
-            if interaction.data:
-                if interaction.channel and interaction.channel.id == config.ticket_channels[guild_name]:
-                    cur.execute(
-                        "INSERT INTO application (discord_name, discord_id, ticket_created) VALUES (?, ?, ?)",
-                        (user_name, user_id, creation_date),
+        if interaction.data:
+            if interaction.channel and interaction.channel.id == config.ticket_channels[guild_name]:
+                cur.execute(
+                    "INSERT INTO application (discord_name, discord_id, ticket_created) VALUES (?, ?, ?)",
+                    (user_name, user_id, creation_date),
+                )
+                conn.commit()
+                await asyncio.sleep(1)
+
+                cur.execute("SELECT id FROM application WHERE discord_id=?", (user_id,))
+                ticket_number = cur.fetchone()[0]
+                category = self.bot.get_channel(category_ids["mod app"])
+
+                if isinstance(category, discord.CategoryChannel):
+                    ticket_channel = await guild.create_text_channel(
+                        f"mod application-{ticket_number}", category=category, topic=f"{interaction.user.id}"
                     )
-                    conn.commit()
-                    await asyncio.sleep(1)
+                else:
+                    raise TypeError(f"category channel is {type(category)}, which is not a ChannelCategory type as required!")
 
-                    cur.execute("SELECT id FROM application WHERE discord_id=?", (user_id,))
-                    ticket_number = cur.fetchone()[0]
-                    category = self.bot.get_channel(category_ids["mod app"])
-
-                    if isinstance(category, discord.CategoryChannel):
-                        ticket_channel = await guild.create_text_channel(
-                            f"mod application-{ticket_number}", category=category, topic=f"{interaction.user.id}"
-                        )
-                    else:
-                        raise TypeError(
-                            f"category channel is {type(category)}, which is not a ChannelCategory type as required!"
-                        )
-
-                    if mod_role:
-                        await ticket_channel.set_permissions(
-                            mod_role,
-                            send_messages=True,
-                            read_messages=True,
-                            add_reactions=True,  # set permissions for the staff team
-                            embed_links=True,
-                            attach_files=True,
-                            read_message_history=True,
-                            external_emojis=True,
-                        )
-                    else:
-                        raise KeyError(
-                            f"Could not get role from guild for roleId {role_ids['mods']}. Cannot set MODS staff role permissions!"  # noqa: E501
-                        )
-
-                    if isinstance(interaction.user, discord.Member):
-                        await ticket_channel.set_permissions(
-                            interaction.user,
-                            send_messages=True,
-                            read_messages=True,
-                            add_reactions=False,  # Set the permissions for the user
-                            embed_links=True,
-                            attach_files=True,
-                            read_message_history=True,
-                            external_emojis=True,
-                        )
-                    else:
-                        raise TypeError("interaction.user is not Member type! Cannot set user permissions")
-
+                if mod_role:
                     await ticket_channel.set_permissions(
-                        guild.default_role, send_messages=False, read_messages=False, view_channel=False
+                        mod_role,
+                        send_messages=True,
+                        read_messages=True,
+                        add_reactions=True,  # set permissions for the staff team
+                        embed_links=True,
+                        attach_files=True,
+                        read_message_history=True,
+                        external_emojis=True,
                     )
-                    embed = discord.Embed(
-                        description=f"Welcome {interaction.user.mention}, \n please run `?app` and follow the directions given by the bot",  # ticket welcome message  # noqa: E501
-                        color=discord.colour.Color.blue(),
-                    )
-                    await ticket_channel.send(embed=embed, view=CloseButton(bot=self.bot))
-                    channel_id = ticket_channel.id
-                    cur.execute("UPDATE application SET ticket_channel = ? WHERE id = ?", (channel_id, ticket_number))
-                    conn.commit()
-                    embed = discord.Embed(
-                        description=f"📬 application was created! Look here --> {ticket_channel.mention}",
-                        color=discord.colour.Color.green(),
+                else:
+                    raise KeyError(
+                        f"Could not get role from guild for roleId {role_ids['mods']}. Cannot set MODS staff role permissions!"  # noqa: E501
                     )
 
-                    await interaction.followup.send(embed=embed, ephemeral=True)
-                    await asyncio.sleep(1)
-                    embed = discord.Embed(
-                        title="Mod Applications",
-                        description="This is where you can apply to be a mod",
-                        color=discord.colour.Color.blue(),
+                if isinstance(interaction.user, discord.Member):
+                    await ticket_channel.set_permissions(
+                        interaction.user,
+                        send_messages=True,
+                        read_messages=True,
+                        add_reactions=False,  # Set the permissions for the user
+                        embed_links=True,
+                        attach_files=True,
+                        read_message_history=True,
+                        external_emojis=True,
                     )
-                    assert interaction.message
-                    await interaction.message.edit(embed=embed, view=MyView(bot=self.bot))  # This will reset the select menu
+                else:
+                    raise TypeError("interaction.user is not Member type! Cannot set user permissions")
+
+                await ticket_channel.set_permissions(
+                    guild.default_role, send_messages=False, read_messages=False, view_channel=False
+                )
+                embed = discord.Embed(
+                    description=f"Welcome {interaction.user.mention}, \n please run `?app` and follow the directions given by the bot",  # ticket welcome message  # noqa: E501
+                    color=discord.colour.Color.blue(),
+                )
+                await ticket_channel.send(embed=embed, view=CloseButton(bot=self.bot))
+                channel_id = ticket_channel.id
+                cur.execute("UPDATE application SET ticket_channel = ? WHERE id = ?", (channel_id, ticket_number))
+                conn.commit()
+                embed = discord.Embed(
+                    description=f"📬 application was created! Look here --> {ticket_channel.mention}",
+                    color=discord.colour.Color.green(),
+                )
+
+                await interaction.followup.send(embed=embed, ephemeral=True)
+                await asyncio.sleep(1)
+                embed = discord.Embed(
+                    title="Mod Applications",
+                    description="This is where you can apply to be a mod",
+                    color=discord.colour.Color.blue(),
+                )
+                assert interaction.message
+                await interaction.message.edit(embed=embed, view=MyView(bot=self.bot))  # This will reset the select menu
