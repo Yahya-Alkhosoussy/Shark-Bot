29a1a998afee71070748ef00b8467486533b6867
Yahya Alkhosoussy <theapexnoob8@gmail.com>
2026-04-02 16:33:32 +0200

    undid change, it was redundant

--- handlers/reactions.py
+++ handlers/reactions.py
@@ -77,7 +77,6 @@ class reaction_handler:
                 for emoji in role_mapping.keys():
                     if existing and message and emoji not in existing:
                         try:
-                            emoji = emoji.replace("\uFE0F", "")
                             await message.add_reaction(emoji)
                         except discord.HTTPException as e:
                             logging.error(f"[RR] could not add reaction {emoji} in {guild_name}. Full Error: {str(e)}")
