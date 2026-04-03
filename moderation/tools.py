710dcb941731b38032b71da7a6bc2939e0373e4b
Yahya Alkhosoussy <theapexnoob8@gmail.com>
2026-04-03 21:47:04 +0200

    Finished and made the tools file work with unit tests

--- moderation/tools.py
+++ moderation/tools.py
@@ -12,10 +12,11 @@ class Moderation(commands.Cog):
 
     @commands.command(name="timeout")
     @is_mod()
-    async def timeout(self, ctx: commands.Context, member: discord.Member, duration: int, *, reason: str = "No reason Provided"):
+    async def timeout(self, ctx: commands.Context, member: discord.Member, duration: int, *, reason: str = "No Reason Provided"):
         assert ctx.guild
         until = dt.timedelta(seconds=duration)
-        await member.timeout(until)
+        await member.timeout(until, reason=reason)
+        await ctx.send(f"{member.name} has been timedout.")
         await self.config.send_discord_mod_log(
             log_message=f"{ctx.author.name} has timed out user ({member.name} {f'(nicknamed: {member.nick})) ' if member.nick else ''}for {duration} seconds. Reason: {reason}",  # noqa: E501
             bot=self.bot,
@@ -25,9 +26,10 @@ class Moderation(commands.Cog):
 
     @commands.command(name="kick")
     @is_mod()
-    async def kick(self, ctx: commands.Context, member: discord.Member, *, reason: str = "No reason Provided"):
+    async def kick(self, ctx: commands.Context, member: discord.Member, *, reason: str = "No Reason Provided"):
         assert ctx.guild
-        await member.kick()
+        await member.kick(reason=reason)
+        await ctx.send(f"{member.name} has been kicked.")
         await self.config.send_discord_mod_log(
             log_message=f"{ctx.author.name} has kicked user {member.name} {f'(nicknamed: {member.nick})) ' if member.nick else ''}from the server. Reason: {reason}",  # noqa: E501
             bot=self.bot,
@@ -37,9 +39,10 @@ class Moderation(commands.Cog):
 
     @commands.command(name="ban")
     @is_mod()
-    async def ban(self, ctx: commands.Context, member: discord.Member, *, reason: str = "No reason Provided"):
+    async def ban(self, ctx: commands.Context, member: discord.Member, *, reason: str = "No Reason Provided"):
         assert ctx.guild
-        await member.ban()
+        await member.ban(reason=reason)
+        await ctx.send(f"{member.name} has been banned.")
         await self.config.send_discord_mod_log(
             log_message=f"{ctx.author.name} has banned user {member.name} {f'(nicknamed: {member.nick})) ' if member.nick else ''}from the server. Reason: {reason}",  # noqa: E501
             bot=self.bot,
