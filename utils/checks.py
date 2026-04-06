e1fd0b8b5788019e34e03c4a677c1b8c3ea12d27
Yahya Alkhosoussy <theapexnoob8@gmail.com>
2026-04-06 21:36:09 +0200

    Fixed an issue where is_mod() helper function would not work due to config not being registered

--- utils/checks.py
+++ utils/checks.py
@@ -1,11 +1,15 @@
 from discord.ext import commands
 import discord
 import exceptions.exceptions as ex
+from pathlib import Path
+from utils.core import AppConfig
+
+CONFIG_PATH = Path(r"config.YAML")
+config = AppConfig(CONFIG_PATH)
 
 def is_mod():
     async def check(ctx: commands.Context):
         assert isinstance(ctx.author, discord.Member)
-        config = ctx.cog.config  # type: ignore This works on run time
         if not config.check_for_mod_role(ctx.author.roles):
             raise ex.InvalidRole("Invalid role, this is only for mods")
         return True
