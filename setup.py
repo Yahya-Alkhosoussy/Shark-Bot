0b45fd17eb750ed7b4b5e6c1532b4bf29630d0bc
Yahya Alkhosoussy <theapexnoob8@gmail.com>
2026-04-13 22:10:21 +0200

    Added a way to install and make sure cmake exists before doing anything.

--- setup.py
+++ setup.py
@@ -1,17 +1,47 @@
 import os
+import shutil
 import subprocess
 import sys
 from pathlib import Path
 
 
+def ensure_cmake():
+    if shutil.which("cmake"):
+        print("CMake already exists at: ", shutil.which("cmake"))
+        return # Cmake exists
+
+    print("cmake not found, installing via pip")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "cmake"])
+
+    # Add Pip's cmake to Path so it can be used imediately
+    cmake_dir = Path(sys.executable).parent / "Scripts" # windows
+    if not cmake_dir.exists():
+        cmake_dir = Path(sys.executable).parent # Unix
+
+    os.environ["PATH"] = str(cmake_dir) + os.pathsep + os.environ["PATH"]
+
+    if not shutil.which("cmake"):
+        raise RuntimeError("CMake installation failed")
+
+    # Make sure it was installed correctly
+    import cmake
+    print(cmake.CMAKE_BIN_DIR) # shows where binary is
+
+    print("CMake installed successfully")
+
+
 def install():
-    print(Path(__file__).parent)
+    ensure_cmake()
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
     subprocess.check_call([sys.executable, "-m", "playwright", "install"])
-    if not os.path.exists("fishing/build"):
-        subprocess.check_call(
-            ["cmake", "-G", "Visual Studio 17", "-S", ".", "-B", "build"], cwd=Path(__file__).parent / "fishing"
-        )
+    os.makedirs(Path(__file__).parent / "fishing" / "build", exist_ok=True)
+    subprocess.check_call(
+        ["cmake", "-G", "Visual Studio 17 2022", "-S", ".", "-B", "build"], cwd=Path(__file__).parent / "fishing"
+    )
+    subprocess.check_call(
+        ["cmake", "--build", "build"],
+        cwd=Path(__file__).parent / "fishing"
+    )
 
     print("Setup complete!")
 
