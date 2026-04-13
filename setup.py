eb09476f3f3eaea9286e5d7bc0298561a6808d80
Yahya Alkhosoussy <theapexnoob8@gmail.com>
2026-04-13 07:48:59 +0200

    Added making the cmake file to the set up

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+import os
 import subprocess
 import sys
+from pathlib import Path
 
 
 def install():
+    print(Path(__file__).parent)
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
     subprocess.check_call([sys.executable, "-m", "playwright", "install"])
+    if not os.path.exists("fishing/build"):
+        subprocess.check_call(
+            ["cmake", "-G", "Visual Studio 17", "-S", ".", "-B", "build"], cwd=Path(__file__).parent / "fishing"
+        )
+
     print("Setup complete!")
 
 
