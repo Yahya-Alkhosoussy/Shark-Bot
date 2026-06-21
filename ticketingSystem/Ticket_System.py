f58a45d3b13aadb7d42e640709bf3a2d6f859647
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-06-21 07:36:52 +0100

    changed type from discord.Client to commands.bot and added the new view to be loaded upon restart

--- ticketingSystem/Ticket_System.py
+++ ticketingSystem/Ticket_System.py
@@ -2,10 +2,13 @@ import logging
 import sqlite3
 
 import discord
+from discord.ext import commands
 
 from ticketingSystem.CloseButton import CloseButton
+from ticketingSystem.customView import customView
 from ticketingSystem.MyView import MyView, config
 from ticketingSystem.TicketOptions import TicketOptions
+from utils.ticketing import TicketingConfig
 
 # ===== LOGGING =====
 handler = logging.FileHandler(filename="tickets.log", encoding="utf-8", mode="a")
@@ -19,7 +22,7 @@ cur = conn.cursor()
 
 
 class TicketSystem:
-    def __init__(self, bot: discord.Client):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
         cur.execute("""CREATE TABLE IF NOT EXISTS 'ticket' (
@@ -54,6 +57,7 @@ class TicketSystem:
         self.bot.add_view(MyView(bot=self.bot))
         self.bot.add_view(CloseButton(bot=self.bot))
         self.bot.add_view(TicketOptions(bot=self.bot))
+        self.bot.add_view(customView(bot=self.bot))
         print("Ticket system loaded | Ticket_System.py")
 
     async def send_ticket_panel(self, channel: discord.TextChannel):
@@ -73,3 +77,56 @@ class TicketSystem:
         logging.info("[TICKETING SYSTEM] Support Ticket Sent")
         guild_name = config.guilds[channel.guild.id]
         config.save_message_id(guild_name, message.id)
+
+    async def send_custom_ticket_panel(self, channel: discord.TextChannel, ticket_config: TicketingConfig):
+
+        embed = discord.Embed(
+            title="Open a custom mod ticket",
+            description="This is where u can open a ticket with certain people",
+            colour=discord.colour.Color.blue(),
+        )
+        message = await channel.send(embed=embed, view=customView(self.bot))
+        guild_name = config.guilds[channel.guild.id]
+        ticket_config.save_message_id(guild_name, message.id, True)
+
+    async def check_for_ticket(self, ticket_config: TicketingConfig, guild_name: str, guild: discord.Guild):
+        print("set up not done")
+
+        logging.info("[TICKETING SYSTEM] Ticket system set up, checking for messages now")
+
+        embed_message_ids = ticket_config.embed_messages
+        if embed_message_ids and embed_message_ids[guild_name] == 0:
+            channel_id = ticket_config.ticket_channels[guild_name]
+            if channel_id is not None or channel_id != 0:
+                channel = guild.get_channel(channel_id)
+                if channel and isinstance(channel, discord.TextChannel):
+                    await self.send_ticket_panel(channel=channel)
+                else:
+                    logging.warning(f"[TICKET SYSTEM] Channel {channel_id} does not exist or is not a TextChannel")
+                    return
+            else:
+                logging.warning(f"[TICKET SYSTEM] Channel ID for {guild_name} is either None or Zero!")
+                return
+
+            logging.info(f"[TICKETING SYSTEM] Ticket embed sent to {guild_name}")
+
+    async def check_for_custom_ticket(self, ticket_config: TicketingConfig, guild_name: str, guild: discord.Guild):
+        print("set up not done")
+
+        logging.info("[TICKETING SYSTEM] Ticket system set up, checking for messages now")
+
+        embed_message_ids = ticket_config.custom_embed_messages
+        if embed_message_ids and embed_message_ids[guild_name] == 0:
+            channel_id = ticket_config.custom_ticket_channels[guild_name]
+            if channel_id is not None or channel_id != 0:
+                channel = guild.get_channel(channel_id)
+                if channel and isinstance(channel, discord.TextChannel):
+                    await self.send_custom_ticket_panel(channel=channel, ticket_config=ticket_config)
+                else:
+                    logging.warning(f"[TICKET SYSTEM] Channel {channel_id} does not exist or is not a TextChannel")
+                    return
+            else:
+                logging.warning(f"[TICKET SYSTEM] Channel ID for {guild_name} is either None or Zero!")
+                return
+
+            logging.info(f"[TICKETING SYSTEM] Ticket embed sent to {guild_name}")
