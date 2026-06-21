f3d54ed5ff92ab4b179419e1655ed68627d062a2
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-06-21 07:38:05 +0100

    added a send_dm option

--- ticketingSystem/CloseButton.py
+++ ticketingSystem/CloseButton.py
@@ -5,8 +5,9 @@ from ticketingSystem.TicketOptions import TicketOptions
 
 # First button for the ticket
 class CloseButton(discord.ui.View):
-    def __init__(self, bot: discord.Client):
+    def __init__(self, bot: discord.Client, send_dm: bool | None = None):
         self.bot = bot
+        self.send_dm = send_dm
         super().__init__(timeout=None)
 
     @discord.ui.button(label="Delete Ticket 🎫", style=discord.ButtonStyle.blurple, custom_id="close")
@@ -17,7 +18,7 @@ class CloseButton(discord.ui.View):
             color=discord.colour.Color.green(),
         )
         await interaction.response.send_message(
-            embed=embed, view=TicketOptions(bot=self.bot)
+            embed=embed, view=TicketOptions(bot=self.bot, send_dm=self.send_dm)
         )  # This wil show the user the Ticket Options view
         assert interaction.message
         await interaction.message.edit(view=self)
