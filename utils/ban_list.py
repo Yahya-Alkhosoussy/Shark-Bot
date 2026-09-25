cf6592f59a474fa6a3b1bd63e5feaaad1a15ddac
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-09-25 21:49:32 +0100

    fixed typo

--- utils/ban_list.py
+++ utils/ban_list.py
@@ -5,7 +5,7 @@ from enum import StrEnum
 class Servers(StrEnum):
     SHARKOCALYPSE = "shark's cult"
     DAVEX = "davex's graveyard"
-    SHARKTROCITY = "The noctunal cavern"
+    SHARKTROCITY = "The nocturnal cavern"
 
 
 class Statuses(StrEnum):
